5f8020243bb1d4e56b39b7e0b3e6a3767f635516
Denis <117849728+007Kasper@users.noreply.github.com>
2022-12-07 17:14:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("It Works")
+
+
+ // Write a JavaScript program that will calculate the price of 30 Phones, where the price of one phone is $119.95 and the tax rate is 5%. 
+
+
+let phones = 30;
+let price = 119.95;
+let taxRate = 5;
+
+let totalPrice = (phones * price) + ((phones * price) * (taxRate / 100));   
+
+console.log("The total price of 30 phones is: $" + totalPrice);
+
+
+
+
+
+
+
